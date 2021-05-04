5947433728fdd41a8efc119ccc274c34d6d9aaeb
Ahmet Melih Öten <moten@rackham3.uppmax.uu.se>
2021-05-04 13:48:50 +0200

    metaquast combined updated

--- 2_sh/3_metaquast_comb.sh
+++ 2_sh/3_metaquast_comb.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 2:00:00
-#SBATCH -J DNA_assembly_evaluation_metaquast_combinedassembly
+#SBATCH -J DNA_assembly_evaluation_metaquast_combined_assembly2_04052021
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user melihoten@gmail.com
 
@@ -14,6 +14,6 @@ module load quast
 
 #Joined pairs
 metaquast.py -t 2 /domus/h1/moten/2021_genanaly/proj_data/1_analyses/0_assembly/dna_assembly_all//final.contigs.fa \
--o /domus/h1/moten/2021_genanaly/proj_data/1_analyses/0_assembly/evaluation_comb/ \
--1 /crex/proj/g2021012/4_Thrash_2017/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz \
--2 /crex/proj/g2021012/4_Thrash_2017/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz
+-o /domus/h1/moten/2021_genanaly/proj_data/1_analyses/0_assembly/evaluation_comb_2/ \
+-1 /crex/proj/g2021012/4_Thrash_2017/DNA_trimmed/*_1.paired.trimmed.fastq.gz \
+-2 /crex/proj/g2021012/4_Thrash_2017/DNA_trimmed/*_2.paired.trimmed.fastq.gz
